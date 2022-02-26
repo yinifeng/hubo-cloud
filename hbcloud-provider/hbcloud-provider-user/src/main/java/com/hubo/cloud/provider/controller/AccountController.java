109fe6ee2e8645ed2b1ab7ad94392a38a5a029ed
yinifeng <78381092@qq.com>
2022-02-26 22:23:58 +0800

    升级springcloud版本

--- hbcloud-provider/hbcloud-provider-user/src/main/java/com/hubo/cloud/provider/controller/AccountController.java
+++ hbcloud-provider/hbcloud-provider-user/src/main/java/com/hubo/cloud/provider/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.hubo.cloud.provider.controller;
 
 import com.hubo.cloud.api.stock.domain.Student;
 import com.hubo.cloud.provider.feign.ShopService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +16,18 @@ import java.util.UUID;
 public class AccountController {
     @Resource
     private ShopService shopService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
     
     @GetMapping("/hello")
     public String helloAccount(){
         System.out.println("===============>gateway request");
         String shop = shopService.getShop("hbcloud-user");
         System.out.println("------>"+shop);
+        System.err.printf("Redis k1 = " + stringRedisTemplate.opsForValue().get("k1"));
+        //stringRedisTemplate.delete()
+
         return UUID.randomUUID().toString();
     }
 
